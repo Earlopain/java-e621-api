d35af462dd5d2b4fd7ce74ae3eda753f8726038d
Earlopain <earlopain@c5h8no4na.net>
2021-02-13 14:41:59 +0100

    Add function to easily get all tags

--- src/main/java/net/c5h8no4na/e621/api/response/partial/Tags.java
+++ src/main/java/net/c5h8no4na/e621/api/response/partial/Tags.java
@@ -1,6 +1,9 @@
 package net.c5h8no4na.e621.api.response.partial;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lombok.Data;
 
@@ -14,4 +17,9 @@ public class Tags {
 	private List<String> invalid;
 	private List<String> lore;
 	private List<String> meta;
+
+	public List<String> getAll() {
+		return Stream.of(general, species, character, copyright, artist, invalid, lore, meta).flatMap(Collection::stream)
+				.collect(Collectors.toList());
+	}
 }
